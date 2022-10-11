e90b83b3035494029c63416f969d2c8af849a6cd
Brooks Susor <susorbrooks@gmail.com>
2022-10-10 20:27:25 -0400

    Create email validation

---
+++ scripts.js
@@ -0,0 +1,25 @@
+const inputs = document.querySelectorAll('input');
+const submit = document.querySelector('button');
+
+const validateEmail = () => {
+  const email = inputs[0];
+  email.setCustomValidity('');
+  email.classList.remove('error');
+  email.classList.remove('valid');
+
+  if (email.checkValidity()) {
+    email.classList.add('valid');
+  }
+
+  if (!email.checkValidity()) {
+    email.classList.add('error');
+    email.setCustomValidity('Enter an appropriate email address.');
+    email.reportValidity();
+  }
+}
+
+email.addEventListener('input', validateEmail);
+
+submit.addEventListener('click', (e) => {
+  e.preventDefault(); 
+})
